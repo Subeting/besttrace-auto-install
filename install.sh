6ff4b41c04b4089236a623283eb58d8053140716
Subeting <68261543+Subeting@users.noreply.github.com>
2021-04-04 23:40:08 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+(apt update;apt install unzip wget -y)||(yum install unzip wget -y)
+wget https://cdn.ipip.net/17mon/besttrace4linux.zip -O /tmp/bt.zip
+unzip -d /tmp /tmp/bt.zip
+chmod 755  /tmp/besttrace
+cp  /tmp/besttrace /bin/ -f
+rm -rf /tmp/besttrace*
+exit 0
