2eea78813492517edbbd82d85258b19f31545e47
Subeting <68261543+Subeting@users.noreply.github.com>
2023-07-05 11:02:08 +0800

    Update nexttrace.sh

--- nexttrace.sh
+++ nexttrace.sh
@@ -2,6 +2,6 @@
 (apt update -y)||true
 (apt install jq -y)||(yum install jq -y)
 newVer=$(curl  "https://api.github.com/repos/nxtrace/Ntrace-core/tags" | jq -r '.[0].name')
-wget https://github.com/nxtrace/Ntrace-core/releases/download/$newVer/nexttrace_linux_amd64 -O /tmp/nexttrace
+wget https://github.com/nxtrace/Ntrace-core/releases/download/v1.1.2/nexttrace_linux_amd64 -O /tmp/nexttrace
 chmod 755 /tmp/nexttrace
 mv /tmp/nexttrace /bin/
