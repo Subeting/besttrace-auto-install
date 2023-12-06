d5a1be3c289e6d7ea50353679bce824fdc8e4c8d
Subeting <68261543+Subeting@users.noreply.github.com>
2023-12-06 15:33:48 +0800

    Update nexttrace.sh

--- nexttrace.sh
+++ nexttrace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash -c "$(curl http://nexttrace-io-leomoe-api-a0.shop/nt_install_v1.sh)"
+curl nxtrace.org/nt |bash
 alias ntr=nexttrace
 #(apt install jq -y)||(yum install jq -y)
 #newVer=$(curl  "https://api.github.com/repos/nxtrace/Ntrace-core/tags" | jq -r '.[0].name')
